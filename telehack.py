d412230871d67011261db0cbd3822d477e3331fe
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2023-05-20 18:41:26 +0530

    Update telehack.py

--- telehack.py
+++ telehack.py
@@ -19,7 +19,7 @@ lightgrey = '\033[47m'
 clear = '\033[0m'
 lgreen = '\033[92m'
 
-banner = lgreen+'''
+banner = orange+'''
 
  ,ggg,         gg                                                              
 dP""Y8a        88                                       ,dPYb,                 
